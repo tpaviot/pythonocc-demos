a89c8086a73a4232438e716ce94e84dc2e5b3b69
Thomas Paviot <tpaviot@gmail.com>
2019-11-27 06:48:44 +0100

    don't load the entire pcd file in memory

--- examples/core_display_point_cloud.py
+++ examples/core_display_point_cloud.py
@@ -29,6 +29,28 @@ from OCC.Core.gp import gp_Pnt
 from OCC.Display.SimpleGui import init_display
 display, start_display, add_menu, add_function_to_menu = init_display()
 
+def pcd_get_number_of_vertices(pcd_filename):
+    """ open the PCD file, read header and get number of vertices.
+    Header looks like:
+    # .PCD v.5 - Point Cloud Data file format
+    VERSION .5
+    FIELDS x y z
+    SIZE 4 4 4
+    TYPE F F F
+    COUNT 1 1 1
+    WIDTH 397
+    HEIGHT 1
+    POINTS 397
+    DATA ascii
+    """
+    f = open(pcd_filename, 'r')
+    # read 8 lines
+    for i in range(8):
+        f.readline()
+    # the 9th line holds the number of points
+    number_of_points = int(f.readline().split()[1])
+    f.close()
+    return number_of_points
 
 def random_points(event=None):
     n_points = 500000
@@ -51,10 +73,20 @@ def random_points(event=None):
     display.FitAll()
 
 def bunny(event=None):
-    pcd_file = open(os.path.join('..', 'assets', 'models', 'bunny.pcd'), 'r').readlines()[10:]
+    start_time = time.time()
+    pcd_file_name = os.path.join('..', 'assets', 'models', 'bunny.pcd')
+    # compute number of lines
+    nbr_of_vertices = pcd_get_number_of_vertices(pcd_file_name)
+    print("Number of vertices :", nbr_of_vertices)
     # create the point_cloud
-    pc = Graphic3d_ArrayOfPoints(len(pcd_file))
-    for line in pcd_file:
+    pc = Graphic3d_ArrayOfPoints(nbr_of_vertices)
+    # fedd it with vertices
+    fp = open(pcd_file_name, 'r')
+    # read 11 lines to skip header
+    for i in range(10):
+        fp.readline()
+    for i in range(nbr_of_vertices):
+        line = fp.readline()
         x, y, z = map(float, line.split())
         pc.AddVertex(x, y, z)
     point_cloud = AIS_PointCloud()
