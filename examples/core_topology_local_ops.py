c0788760458f2e1ca12322171ab80524bc97b8c1
Thomas Paviot <tpaviot@gmail.com>
2020-02-25 14:55:41 +0100

    Fixed core_topology_local_ops

--- examples/core_topology_local_ops.py
+++ examples/core_topology_local_ops.py
@@ -161,7 +161,7 @@ def thick_solid(event=None):
 def offset_cube(event=None):
     S2 = BRepPrimAPI_MakeBox(gp_Pnt(300, 0, 0), 220, 140, 180).Shape()
     offsetB = BRepOffsetAPI_MakeOffsetShape(S2, -20, 0.01, BRepOffset_Skin, False, False, GeomAbs_Arc)
-    offB = display.DisplayColoredShape(S2, 'BLUE')
+    offB = display.DisplayColoredShape(S2, 'BLUE')[0]
     display.Context.SetTransparency(offB, 0.3, True)
     display.DisplayColoredShape(offsetB.Shape(), 'GREEN')
     display.FitAll()
