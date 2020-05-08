4028009ce16882b8cbc579d30ac1e52c2e849cb3
godjenka <ken.nsiempba@mail.mcgill.ca>
2020-05-08 01:11:44 -0400

    Update Core_BRepOffsetAPI_BRepOffsetAPI_MakePipeShell.py

--- examples/Core_BRepOffsetAPI_BRepOffsetAPI_MakePipeShell.py
+++ examples/Core_BRepOffsetAPI_BRepOffsetAPI_MakePipeShell.py
@@ -6,10 +6,8 @@ from OCC.Core.TColgp import TColgp_Array1OfPnt
 from OCC.Core.Law import Law_Linear
 from OCC.Core.gp import gp_Circ, gp_Pnt, gp_ZOX
 display, start_display, add_menu, add_function_to_menu = init_display()
-
 # Main function
 def Thicken_spline(event=None):
-    
     # Creation of points for the spine
     array = TColgp_Array1OfPnt(1, 5)
     array.SetValue(1, gp_Pnt(1, 4,0))
@@ -17,13 +15,11 @@ def Thicken_spline(event=None):
     array.SetValue(3, gp_Pnt(3, 3,0))
     array.SetValue(4, gp_Pnt(4, 3,0))
     array.SetValue(5, gp_Pnt(5, 5,0))
-    
     # Creation of a Bezier Curve as the spine
     Bz_curv = Geom_BezierCurve(array)
     Bz_curv_Edge = BRepBuilderAPI_MakeEdge(Bz_curv).Edge()
     Bz_curv_Wire = BRepBuilderAPI_MakeWire(Bz_curv_Edge).Wire()
     display.DisplayShape(Bz_curv_Wire)
-    
     # Creation of profile to sweep along the spine
     circle = gp_Circ(gp_ZOX(), 1)
     circle.SetLocation(array[0])
@@ -35,7 +31,6 @@ def Thicken_spline(event=None):
     Law_f.Set(0,0.5,1,1)
     brep1.SetLaw(circle_Wire,Law_f,False, True)
     return brep1.Shape()
-
 # Display section
 if __name__ == '__main__':
     display.DisplayShape(Thicken_spline())
