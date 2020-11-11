84273a8b7a3cb948587162f25488721dc1f34351
tpaviot <tpaviot@gmail.com>
2020-11-11 15:44:04 +0100

    added another example with camera and eye

---
+++ examples/core_visualization_camera_2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+##Copyright 2016 Jelle Feringa (jelleferinga@gmail.com)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU Lesser General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU Lesser General Public License for more details.
+##
+##You should have received a copy of the GNU Lesser General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+
+from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
+from OCC.Display.SimpleGui import init_display
+
+display, start_display, add_menu, add_function_to_menu = init_display()
+
+# load step file
+the_shape = BRepPrimAPI_MakeBox(300, 200, 100).Shape()
+display.DisplayShape(the_shape, update=True)
+
+
+def animate_viewpoint():
+    cam = display.View.Camera()  # type: Graphic3d_Camera
+
+    center = cam.Center()
+    eye = cam.Eye()
+    eye.SetX(0.)
+    eye.SetZ(0.)
+    eye.SetZ(0.)
+    cam.SetEye(eye)
+
+    display.FitAll()
+    display.Context.UpdateCurrentViewer()
+
+    for i in range(100):
+        eye.SetY(eye.Y() + i)
+        cam.SetEye(eye)
+        display.Context.UpdateCurrentViewer()
+
+add_menu("camera")
+add_function_to_menu("camera", animate_viewpoint)
+
+start_display()
