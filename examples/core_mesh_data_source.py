b7241a6064433dfc479f3267b344f9f5e755164b
tpaviot <tpaviot@gmail.com>
2021-04-13 17:54:30 +0200

    Add first draft for a mesh data source example

---
+++ examples/core_mesh_data_source.py
@@ -0,0 +1,41 @@
+##Copyright 2021 Thomas Paviot (tpaviot@gmail.com)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU Lesser General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU Lesser General Public License for more details.
+##
+##You should have received a copy of the GNU Lesser General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+from OCC.Core.MeshDataSource import Mesh_DataSource
+from OCC.Core.RWStl import rwstl_ReadFile
+from OCC.Core.MeshVS import *
+
+from OCC.Display.SimpleGui import init_display
+
+stl_filename = os.path.join('..', 'assets', 'models', 'fan.stl')
+
+a_stl_mesh = rwstl_ReadFile(stl_filename)
+
+a_data_source = Mesh_DataSource(a_stl_mesh)
+
+a_mesh_prs = MeshVS_Mesh()
+a_mesh_prs.SetDataSource(a_data_source)
+a_builder = MeshVS_MeshPrsBuilder(a_mesh_prs)
+
+a_mesh_prs.AddBuilder (a_builder, True)
+
+display, start_display, add_menu, add_function_to_menu = init_display()
+
+display.Context.Display(a_mesh_prs, True)
+start_display()
