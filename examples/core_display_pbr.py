c016adad373b414ee0ae7dd4e95b7f878016d3b2
tpaviot <tpaviot@gmail.com>
2021-04-17 08:05:07 +0200

    Update PBR example

--- examples/core_display_pbr.py
+++ examples/core_display_pbr.py
@@ -21,10 +21,11 @@ from OCC.Display.SimpleGui import init_display
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox, BRepPrimAPI_MakeCone
 from OCC.Core.Graphic3d import (Graphic3d_NOM_PLASTIC, Graphic3d_NOM_ALUMINIUM,
                                 Graphic3d_TOSM_PBR, Graphic3d_TOSM_PBR_FACET,
-                                Graphic3d_TOSM_FRAGMENT, Graphic3d_TOSM_VERTEX,
-                                Graphic3d_PBRMaterial, Graphic3d_MaterialAspect)
+                                Graphic3d_TOSM_VERTEX,
+                                Graphic3d_PBRMaterial, Graphic3d_MaterialAspect,
+                                Graphic3d_BSDF, Graphic3d_Vec3)
 from OCC.Core.V3d import V3d_SpotLight, V3d_XnegYnegZpos, V3d_AmbientLight, V3d_DirectionalLight
-from OCC.Core.Quantity import Quantity_Color, Quantity_NOC_WHITE, Quantity_NOC_CORAL2, Quantity_NOC_BROWN, Quantity_NOC_GRAY
+from OCC.Core.Quantity import Quantity_Color, Quantity_NOC_WHITE, Quantity_NOC_CORAL2
 from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Cut
 from OCC.Core.gp import gp_Vec, gp_Pnt, gp_Dir
 
@@ -44,39 +45,41 @@ display, start_display, add_menu, add_function_to_menu = init_display()
 # ambient light
 ambient_light = V3d_AmbientLight(Quantity_Color(Quantity_NOC_WHITE))
 display.Viewer.AddLight(ambient_light)
-
 # directional light
 dir_light = V3d_DirectionalLight(gp_Dir(0,0,1), Quantity_Color(Quantity_NOC_WHITE))
 display.Viewer.AddLight(dir_light)
-
-# create one spotlight
+# light
 spot_light = V3d_SpotLight(gp_Pnt(-100, -100, 100),
                            V3d_XnegYnegZpos, Quantity_Color(Quantity_NOC_WHITE))
 ## display the spotlight in rasterized mode
 display.Viewer.AddLight(spot_light)
 
-display.View.SetLightOn()
-
-# first create the PBR material
-pbr_mat = Graphic3d_PBRMaterial()
-print(dir(pbr_mat))
-pbr_mat.SetMetallic(0.8)
-pbr_mat.SetRoughness(0.5)
-# modifies albedo color
-#pbr_mat.SetColor(Quantity_Color(Quantity_NOC_GRAY))
-
-# then the material aspect
+# first create the material for the bottle
+pbr_mat_1 = Graphic3d_PBRMaterial()
+pbr_mat_1.SetMetallic(0.8)
+pbr_mat_1.SetRoughness(0.5)
 alu_pbr_aspect = Graphic3d_MaterialAspect(Graphic3d_NOM_ALUMINIUM)
-alu_pbr_aspect.SetPBRMaterial(pbr_mat)
+alu_pbr_aspect.SetPBRMaterial(pbr_mat_1)
+alu_pbr_aspect.SetBSDF(Graphic3d_BSDF().CreateMetallicRoughness(pbr_mat_1))
 
-#print(dir(pbr_mat))
+
+# then the glass
+pbr_mat_2 = Graphic3d_PBRMaterial()
+glass_pbr_aspect = Graphic3d_MaterialAspect(Graphic3d_NOM_PLASTIC)
+glass_pbr_aspect.SetPBRMaterial(pbr_mat_2)
+# the BSDF glass
+weights = Graphic3d_Vec3(0.5, 6., 1.)
+absorption_color = Graphic3d_Vec3(0.8, 0.8, 0.8)
+absorption_coef = 0.6
+refraction_index = 2.2
+glass_pbr_aspect.SetBSDF(Graphic3d_BSDF().CreateGlass(weights, absorption_color, absorption_coef, refraction_index))
+
 display.EnableAntiAliasing()
 display.DisplayShape(bottle, material=alu_pbr_aspect)
 display.DisplayShape(table, material=Graphic3d_NOM_PLASTIC, color=Quantity_NOC_CORAL2)
 display.DisplayShape(translated_glass,
-                     material=Graphic3d_NOM_PLASTIC,
-                     color=Quantity_NOC_BROWN,
-                     transparency=0.6,
+                     material=glass_pbr_aspect,
+                     transparency=0.8,
                      update=True)
 
 def pbr(event=None):
@@ -91,14 +94,6 @@ def phong(event=None):
     display.View.SetShadingModel(Graphic3d_TOSM_VERTEX)
     display.View.Redraw()
 
-def gouraud(event=None):
-    display.View.SetShadingModel(Graphic3d_TOSM_FRAGMENT)
-    display.View.Redraw()
-
-def rasterization(event=None):
-    display.SetRasterizationMode()
-    display.View.Redraw()
-
 def exit(event=None):
     sys.exit(0)
 
@@ -107,8 +102,6 @@ def exit(event=None):
 
 if __name__ == '__main__':
     add_menu('PBR')
-    add_function_to_menu('PBR', rasterization)
-    add_function_to_menu('PBR', gouraud)
     add_function_to_menu('PBR', phong)
     add_function_to_menu('PBR', pbr)
     add_function_to_menu('PBR', pbr_facet)
