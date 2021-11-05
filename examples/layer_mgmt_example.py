e81f7f2c0e65b331e49d7a45fa4bfd51a4041c3c
Tanneguy <tanneguydv@gmail.com>
2021-11-05 16:08:06 +0100

    Update layer_mgmt_example.py

--- examples/layer_mgmt_example.py
+++ examples/layer_mgmt_example.py
@@ -1,51 +1,38 @@
 from OCC.Display.SimpleGui import init_display
-display,start_display, add_menu,add_functionto_menu = init_display()
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
+from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Trsf
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_Transform
+from OCC.Extend.LayerMgmt import Layer
+display,start_display, add_menu,add_functionto_menu = init_display()
 
-box = BRepPrimAPI_MakeBox(10,10,100).Shape()
-box2 = BRepPrimAPI_MakeBox(100,10,10).Shape()
-box3 = BRepPrimAPI_MakeBox(10,100,10).Shape()
+box1 = BRepPrimAPI_MakeBox(gp_Pnt(0, 0, 10), 10, 10, 100).Shape()
+box2 = BRepPrimAPI_MakeBox(gp_Pnt(), 100, 10, 10).Shape()
+box3 = BRepPrimAPI_MakeBox(10, 100, 10).Shape()
+box4 = BRepPrimAPI_MakeBox(gp_Pnt(100, 100, 0), 10, 10, 100).Shape()
+box5 = BRepPrimAPI_MakeBox(gp_Pnt(0, 100, 0), 100, 10, 10).Shape()
+box6 = BRepPrimAPI_MakeBox(gp_Pnt(100, 0, 0), 10, 100, 10).Shape()
+trns = gp_Trsf()
+trns.SetTranslation(gp_Vec(0, 0, 110))
 
-class Layer():
+layer1 = Layer(display, color=123)
+layer1.add(box1)
 
-    def __init__(self):
-        self.clear()
-
-    def create(self, shape, color=0):
-        self.color = color
-        self.to_display = display.DisplayShape(shape, color=self.color)[0]
-        self.count += 1
-        self.list_to_display.append(self.to_display)
-        display.Context.Erase(self.to_display, False)
-
-    def show(self):
-        for shape in self.list_to_display:
-            display.Context.Display(shape, True)
-
-    def hide(self):
-        for shape in self.list_to_display:
-            display.Context.Erase(shape, False)
-
-    def add(self, shape):
-        self.to_display = {self.count}
-        self.to_display = display.DisplayShape(shape, color=self.color)[0]
-        self.list_to_display.append(self.to_display)
-        display.Context.Erase(self.to_display, False)
-
-    def clear(self):
-        self.list_to_display = []
-        self.count = 0
-        self.to_display = {self.count}
-
-
-layer1 = Layer()
-layer1.create(box)
-layer1.add(box2)
-layer1.show()
-
-layer2 = Layer()
-layer2.create(box3, 123)
+layer2 = Layer(display, box4, 86)
+layer2.add(box5)
 layer2.show()
+
+layer3 = Layer(display, box2, 76)
+layer3.add(box3)
+layer3.add(box6)
+
+layer3.join(layer1, True)
+layer3.show()
+layer3_shapes = layer3.get_shapes()
+layer4 = Layer(display, color=32)
+for shape in layer3_shapes:
+    translated = BRepBuilderAPI_Transform(shape, trns).Shape()
+    layer4.add(translated)
+layer4.show()
 
 display.FitAll()
 start_display()
