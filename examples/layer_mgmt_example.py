dc3f9f3721acf2ee126410ecc252e25e38937e7b
Tanneguy <tanneguydv@gmail.com>
2021-11-05 16:08:52 +0100

    Update layer_mgmt_example.py

--- examples/layer_mgmt_example.py
+++ examples/layer_mgmt_example.py
@@ -1,3 +1,20 @@
+##Copyright 2021 Tanneguy de Villemagne (tanneguydv@gmail.com)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU Lesser General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU Lesser General Public License for more details.
+##
+##You should have received a copy of the GNU Lesser General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+
 from OCC.Display.SimpleGui import init_display
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
 from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Trsf
