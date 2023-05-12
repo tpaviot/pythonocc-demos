b9638cffeada28a96ae06e7494e465ecc7064485
tpaviot <tpaviot@gmail.com>
2023-05-12 05:56:50 +0200

    clean up core_geometry_oriented_bounding_box

--- examples/core_geometry_oriented_bounding_box.py
+++ examples/core_geometry_oriented_bounding_box.py
@@ -14,63 +14,64 @@
 ##
 ##You should have received a copy of the GNU Lesser General Public License
 ##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
-
 import random
 
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
 from OCC.Core.gp import gp_Pnt, gp_Ax2, gp_Dir, gp_XYZ
-from OCC.Core.BRepBndLib import brepbndlib_AddOBB
+from OCC.Core.BRepBndLib import brepbndlib
 from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeVertex
 from OCC.Core.Bnd import Bnd_OBB
+from OCC.Core.BRepTools import breptools_Read
+from OCC.Core.TopoDS import TopoDS_Shape
+from OCC.Core.BRep import BRep_Builder
 
 from OCC.Display.SimpleGui import init_display
 
 display, start_display, add_menu, add_function_to_menu = init_display()
 
 
-def ConvertBndToShape(theBox):
-    aBaryCenter = theBox.Center()
-    aXDir = theBox.XDirection()
-    aYDir = theBox.YDirection()
-    aZDir = theBox.ZDirection()
-    aHalfX = theBox.XHSize()
-    aHalfY = theBox.YHSize()
-    aHalfZ = theBox.ZHSize()
+def convert_bnd_to_shape(the_box):
+    """Converts a bounding box to a box shape."""
+    barycenter = the_box.Center()
+    x_dir = the_box.XDirection()
+    y_dir = the_box.YDirection()
+    z_dir = the_box.ZDirection()
+    half_x = the_box.XHSize()
+    half_y = the_box.YHSize()
+    half_z = the_box.ZHSize()
 
-    ax = gp_XYZ(aXDir.X(), aXDir.Y(), aXDir.Z())
-    ay = gp_XYZ(aYDir.X(), aYDir.Y(), aYDir.Z())
-    az = gp_XYZ(aZDir.X(), aZDir.Y(), aZDir.Z())
-    p = gp_Pnt(aBaryCenter.X(), aBaryCenter.Y(), aBaryCenter.Z())
-    anAxes = gp_Ax2(p, gp_Dir(aZDir), gp_Dir(aXDir))
-    anAxes.SetLocation(gp_Pnt(p.XYZ() - ax * aHalfX - ay * aHalfY - az * aHalfZ))
-    aBox = BRepPrimAPI_MakeBox(anAxes, 2.0 * aHalfX, 2.0 * aHalfY, 2.0 * aHalfZ).Shape()
-    return aBox
+    x_vec = gp_XYZ(x_dir.X(), x_dir.Y(), x_dir.Z())
+    y_vec = gp_XYZ(y_dir.X(), y_dir.Y(), y_dir.Z())
+    z_vec = gp_XYZ(z_dir.X(), z_dir.Y(), z_dir.Z())
+    point = gp_Pnt(barycenter.X(), barycenter.Y(), barycenter.Z())
+    axes = gp_Ax2(point, gp_Dir(z_dir), gp_Dir(x_dir))
+    axes.SetLocation(
+        gp_Pnt(point.XYZ() - x_vec * half_x - y_vec * half_y - z_vec * half_z)
+    )
+    box = BRepPrimAPI_MakeBox(axes, 2.0 * half_x, 2.0 * half_y, 2.0 * half_z).Shape()
+    return box
 
 
 # compute the oriented bounding box of a point cloud
 obb1 = Bnd_OBB()
-n = 10
-for _ in range(n):
+num_points = 10
+for _ in range(num_points):
     x = random.uniform(100, 500)
     y = random.uniform(100, 500)
     z = random.uniform(100, 500)
     p = BRepBuilderAPI_MakeVertex(gp_Pnt(x, y, z)).Shape()
     display.DisplayShape(p)
-    brepbndlib_AddOBB(p, obb1)
-obb_shape1 = ConvertBndToShape(obb1)
+    brepbndlib.AddOBB(p, obb1)
+obb_shape1 = convert_bnd_to_shape(obb1)
 display.DisplayShape(obb_shape1, transparency=0.5)
 
 # then loads a brep file and computes the optimal bounding box
-from OCC.Core.BRepTools import breptools_Read
-from OCC.Core.TopoDS import TopoDS_Shape
-from OCC.Core.BRep import BRep_Builder
-
 cylinder_head = TopoDS_Shape()
 builder = BRep_Builder()
 breptools_Read(cylinder_head, "../assets/models/cylinder_head.brep", builder)
 obb2 = Bnd_OBB()
-brepbndlib_AddOBB(cylinder_head, obb2, True, True, True)
-obb_shape2 = ConvertBndToShape(obb2)
+brepbndlib.AddOBB(cylinder_head, obb2, True, True, True)
+obb_shape2 = convert_bnd_to_shape(obb2)
 display.DisplayShape(cylinder_head)
 display.DisplayShape(obb_shape2, transparency=0.5, update=True)
 
