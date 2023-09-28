69f80423646b437df84da8c09a0b53059f7416d3
Thomas Paviot <tpaviot@gmail.com>
2023-09-28 07:25:33 +0200

    update ply exporter to api change

--- examples/core_export_ply_single_shape.py
+++ examples/core_export_ply_single_shape.py
@@ -15,44 +15,10 @@
 ##You should have received a copy of the GNU Lesser General Public License
 ##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
 
-from OCC.Core.TDocStd import TDocStd_Document
-from OCC.Core.TCollection import TCollection_ExtendedString, TCollection_AsciiString
-from OCC.Core.XCAFDoc import (
-    XCAFDoc_DocumentTool_ShapeTool,
-    XCAFDoc_DocumentTool_LayerTool,
-)
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeSphere
-from OCC.Core.TColStd import TColStd_IndexedDataMapOfStringString
-from OCC.Core.Message import Message_ProgressRange
-from OCC.Core.BRepMesh import BRepMesh_IncrementalMesh
-from OCC.Core.BRepTools import breptools_Clean
 
-# GLTF export
-from OCC.Core.RWPly import RWPly_CafWriter
+from OCC.Extend.DataExchange import write_ply_file
 
 # create the shapeto export
 shp = BRepPrimAPI_MakeSphere(60.0).Shape()
-
-# create a document
-doc = TDocStd_Document("pythonocc-doc")
-shape_tool = XCAFDoc_DocumentTool_ShapeTool(doc.Main())
-layer_tool = XCAFDoc_DocumentTool_LayerTool(doc.Main())
-
-# mesh shape
-breptools_Clean(shp)
-
-# Triangulate
-msh_algo = BRepMesh_IncrementalMesh(shp, True)
-msh_algo.Perform()
-
-sub_shape_label = shape_tool.AddShape(shp)
-
-# metadata
-a_file_info = TColStd_IndexedDataMapOfStringString()
-a_file_info.Add(
-    TCollection_AsciiString("Authors"), TCollection_AsciiString("pythonocc")
-)
-
-rwply_writer = RWPly_CafWriter("sphere.ply")
-pr = Message_ProgressRange()
-rwply_writer.Perform(doc, a_file_info, pr)
+write_ply_file(shp, "sphere.ply")
